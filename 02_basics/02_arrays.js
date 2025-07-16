429aaae781d6aa88310e4aca0d32d45ea3913106
Rishabh Bahuguna <122173160+rbahuguna10@users.noreply.github.com>
2025-07-16 09:31:09 +0000

    arrays

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,28 @@
+const marvel_heros = ["thor", "Ironman", "spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
+
+// marvel_heros.push(dc_heros)
+
+// console.log(marvel_heros);              // [ 'thor', 'Ironman', 'spiderman', [ 'superman', 'flash', 'batman' ] ]
+// console.log(marvel_heros[3][1]);        // flash
+
+// To solve the above problem we have concat()
+// const allHeros = marvel_heros.concat(dc_heros)   // Combines two or more arrays & returns a new array
+// console.log(allHeros);                           // [ 'thor', 'Ironman', 'spiderman', 'superman', 'flash', 'batman' ]
+
+// Alternative of concat() -> spread operator (more preferred)
+const all_new_heros = [...marvel_heros, ...dc_heros]
+console.log(all_new_heros);                // [ 'thor', 'Ironman', 'spiderman', 'superman', 'flash', 'batman' ]
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+const real_another_array = another_array.flat(Infinity)  // returns a new array with all sub-array elements concatenated into it recursively up to the specified depth.
+console.log(real_another_array)
+
+console.log(Array.isArray("Hitesh"))           // false
+console.log(Array.from("Hitesh"))              // [ 'H', 'i', 't', 'e', 's', 'h' ] , converts the given value to an array
+console.log(Array.from({name: "hitesh"}))      // [] , because we haven't mentioned which one to be converted 'key' or 'value' 
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+console.log(Array.of(score1, score2, score3))  // [ 100, 200, 300 ]
