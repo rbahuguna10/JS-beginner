01cdff02704cdccea70d668fb99776f4f92bb55a
Rishabh Bahuguna <122173160+rbahuguna10@users.noreply.github.com>
2025-07-19 04:44:27 +0000

    objects...

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -30,3 +30,28 @@ const obj4 = {5: "a", 6: "b"}
 // Alternative of Object.assign() -> spread operator (more preferred):-
 const obj3 = {...obj1, ...obj2, ...obj4}
 console.log(obj3)                               // { '1': 'a', '2': 'b', '3': 'a', '4': 'b', '5': 'a', '6': 'b' }
+
+// Array of Objects (used to fetch values from DB):-
+const users = [
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+]
+
+// users[1].email                                       // old way to fetch values
+
+console.log(tinderUser)                                 // { id: '123abc', name: 'Sammy', isLoggedIn: false }
+console.log(Object.keys(tinderUser))                    // [ 'id', 'name', 'isLoggedIn' ]
+console.log(Object.values(tinderUser))                  // [ '123abc', 'Sammy', false ]
+console.log(Object.entries(tinderUser))                 // [ [ 'id', '123abc' ], [ 'name', 'Sammy' ], [ 'isLoggedIn', false ] ]
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'))    // to check if the object has an specific property or not
